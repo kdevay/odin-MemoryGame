fdf9708d8eec4a8f64a016c5d56178b43a26a416
Katherine de Vay <katdevay2@gmail.com>
2023-02-13 15:44:07 -0600

    Debug modal close/reset buttons

--- memory-game/src/components/Heading.js
+++ memory-game/src/components/Heading.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export default function Heading(props) {
-    const {setIsHelp, score, topScore} = props;
+    const {handleHelp, score, topScore} = props;
+
     return(
         <div id='heading'>
-            <button onClick={handleHelp}>?</button>
+            <button id='help' onClick={handleHelp}>?</button>
             <h1 id='title'>Memory Game</h1>
             <div id='scoreBoard'>
                 <h5>Score: {score}</h5>
--- memory-game/src/components/Modal.js
+++ memory-game/src/components/Modal.js
@@ -1,30 +1,62 @@
 import React from 'react';
 
 export default function Modal(props) {
-    const {isHelp, score, topScore, closeModal, resetGame} = props;
+    const {isHelp, score, topScore, close, reset} = props;
+
     //onClick={setIsHelp}
-    if(isHelp){
+    if (isHelp){
         return(
+        <div id='modalShadow' onClick={close}>
             <div className='modal'>
-                <button className='close' onClick={closeModal}>X</button>
-                <h4>How to play:</h4>
-                <h5>The goal is to click each of the 12 tiles exactly once</h5>
-                <h5>Every time a tile is clicked, the board shuffles.</h5>
-                <h5>For every new tile you click, you get one point.</h5>
-                <h5>If you click the same tile more than once, you lose a point.</h5>
-                <button onClick={resetGame}>Reset Game</button>
+                <button className='close' onClick={close}>X</button><br></br>
+                <div className='modalCenter'>
+                    <div>
+                        <h3>How to play:</h3>
+                        <h5>The goal is to click each of the 12 tiles exactly once</h5>
+                        <h5>Every time a tile is clicked, the board shuffles.</h5>
+                        <h5>For every new tile you click, you get one point.</h5>
+                        <h5>If you click the same tile more than once, you lose a point.</h5>
+                    </div>
+                    <br></br><button onClick={reset}>Reset Game</button>
+                </div>
             </div>
+        </div>
         )
-    }
-    if(topScore > score){
+    } else if (topScore > score){
         return(
-            <div className='close' className='modal'>
-                <button onClick={resetGame}>X</button>
-                <h4>Game Over!</h4>
-                <h5>Your score: {score}</h5>
-                <h5>Top score: {topScore}</h5>
-                <button onClick={resetGame}>New Game</button>
+            <div id='modalShadow' onClick={reset}>
+                <div className='modal'>
+                    <button className='close' onClick={reset}>X</button><br></br>
+                    <div className='modalCenter'>
+                        <div>
+                                <h3>Game Over!</h3>
+                                <h5>Your score: {score}</h5>
+                                <h5>Top score: {topScore}</h5>
+                        </div>
+                        <br></br><button onClick={reset}>New Game</button>
+                    </div>
+                </div>
             </div>
         )
     }
+    return(
+        <div id='modalShadow' onClick={reset}>
+            <div className='modal'>
+                <button className='close' onClick={reset}>X</button><br></br>
+                <div className='modalCenter'>
+                    <div>
+                        <h3>Game over!</h3>
+                        { 
+                            score === 12 
+                            ? <h4>You won a perfect score!</h4>
+                            : <h4> New Top Score!</h4>
+                        }
+                        <h5>Your score: {score}</h5>
+                        <h5>Top score: {topScore}</h5>
+                    </div>
+                    <br></br><button onClick={reset}>New Game</button>
+                </div>
+            </div>
+        </div>
+    )
 }
